f68bb8d1bd7b3d7beaa78fc40e5578e1fd4676c3
David <David@Davids-MacBook-Pro-2.local>
2016-08-31 10:50:36 -0500

    upload

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+
+var source = {
+	localhost: {
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'	
+	},
+	livehost: {
+		port: 3306,
+		host: 'd6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'vnhzm1oaaipbpuef',
+		password: 'ayzw4lhggczakoav',
+		database: 'mhnmixpemvns73fj'
+	}
+}
+
+var connection = mysql.createConnection(source.localhost);
+
+connection.connect(function(err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected to MySQL Database as id ' + connection.threadId);
+});
+
+module.exports = connection;
